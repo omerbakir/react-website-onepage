b4b309771757d9c4bd49a6806199faa8ee616b06
omerfarukbakir <omerfarukbakir@gmail.com>
2021-12-16 15:11:53 +0300

    Sidebar update and bugs fixed

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -20,7 +20,7 @@ const HeroSection = () => {
     }
 
     return (
-        <HeroContainer>
+        <HeroContainer id="home">
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type="video/mp4"/>
             </HeroBg>
@@ -35,7 +35,9 @@ const HeroSection = () => {
                             onMouseEnter={onHover}
                             onMouseLeave={onHover}
                             primary="true"
-                            dark="true">
+                            dark="true"
+                            smooth={true} duration={500} spy={true}
+                            exact="true" offset={-80}>
                         Get started {hover ? <ArrowForward/> : <ArrowRight/>}
                     </Button>
                 </HeroBtnWrapper>
